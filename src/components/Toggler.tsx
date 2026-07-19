b1556a448a84ed06c895c1689ddc8dfefa051617
paruripariram <zahovaevvladislav@gmail.com>
2026-07-19 14:11:29 +0300

    make toggler universal

--- src/components/Toggler.tsx
+++ src/components/Toggler.tsx
@@ -1,18 +1,43 @@
-'use client'
+"use client";
 
-type MediaType = "movie" | "tv"
+import React from "react";
+
+interface TogglerOption {
+    value: string;
+    label: string | React.ReactNode;
+}
 interface TogglerProps {
-    type: MediaType
-    typeHandler: () => void
+    options: TogglerOption[];
+    value: string;
+    optionHandler: (value: string) => void;
 }
 
-export default function Toggler({type, typeHandler}: TogglerProps) {
+export default function Toggler({
+    options,
+    value,
+    optionHandler,
+}: TogglerProps) {
+    const activeIndex = options.findIndex((opt) => opt.value === value);
 
+    const sliderWidth = 100 / options.length;
     return (
-        <div className="flex items-center justify-between w-45 h-10 bg-bgcolor rounded-full relative">
-            <div className={`absolute w-1/2 h-10 bg-back-link-color rounded-full z-2 transition-transform duration-300 ${type === "movie" ? "translate-x-0" : "translate-x-full"}`}></div>
-            <button onClick={() => typeHandler()} className={`relative z-3 rounded-full flex-1 h-full font-extrabold transition-colors duration-300 ${type === "movie" ? "text-primary" : "text-gray-500"}`}>Фильмы</button>
-            <button onClick={() => typeHandler()} className={`relative z-3 rounded-full flex-1 h-full font-extrabold transition-colors duration-300 ${type === "tv" ? "text-primary" : "text-gray-500"}`}>Сериалы</button>
+        <div className="flex items-center justify-between h-10 bg-bgcolor rounded-full relative">
+            <div
+                className={`absolute h-full bg-back-link-color rounded-full z-2 transition-transform duration-300 ease-in-out`}
+                style={{
+                    width: `${sliderWidth}%`,
+                    transform: `translateX(${activeIndex * 100}%)`,
+                }}
+            ></div>
+            {options.map((option) => (
+                <button
+                    key={option.value}
+                    onClick={() => optionHandler(option.value)}
+                    className={`relative z-3 rounded-full flex-1 h-full flex items-center justify-center font-extrabold transition-colors duration-300 ${value === option.value ? "text-primary" : "text-gray-500"}`}
+                >
+                    {option.label}
+                </button>
+            ))}
         </div>
-    )
+    );
 }
