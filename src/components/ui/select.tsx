42b38027e9e28cf1d2a54790166035eeaa6e7352
paruripariram <zahovaevvladislav@gmail.com>
2026-08-16 01:11:12 +0300

    fix select on mobile

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -70,7 +70,7 @@ function SelectContent({
                 data-slot="select-content"
                 data-align-trigger={position === "item-aligned"}
                 className={cn(
-                    "relative z-50 max-h-96 min-w-36 overflow-hidden rounded-xl border border-white/10 bg-bgcolor backdrop-blur-md text-white shadow-2xl p-1 duration-100 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+                    "relative z-100 max-h-96 min-w-36 overflow-hidden rounded-xl border border-white/10 bg-bgcolor backdrop-blur-md text-white shadow-2xl p-1 duration-100 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
                     position === "popper" &&
                         "data-[side=bottom]:translate-y-1.5 data-[side=left]:-translate-x-1.5 data-[side=right]:translate-x-1.5 data-[side=top]:-translate-y-1.5",
                     className,
